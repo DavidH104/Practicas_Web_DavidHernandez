89b0ed3512dd6a76dc34ca34da5515ae0398de00
¨David¨ <daviddejesushh@gmail.com>
2026-01-28 18:05:26 -0600

    aa

--- practica04-angular/src/app/app.ts
+++ practica04-angular/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
